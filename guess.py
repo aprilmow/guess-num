66dc2b5f2fdb8f65df5b17976d85e25968640a61
April Tseng <aprilmow1018129@gmail.com>
2019-03-04 18:04:37 +0800

    add counts

--- guess.py
+++ guess.py
@@ -1,12 +1,17 @@
 import random
 
 r = random.randint(1, 100)
+
+count = 0
 while True:
+	count += 1 #count = count + 1
 	guess = int(input('guess a number:'))
 	if guess == r:
 		print('correct!')
+		print('this is your', count, 'times gussing')
 		break
 	elif guess > r:
 		print('too big')
 	else:
 		print('too small')
+	print('this is your', count, 'times gussing')
